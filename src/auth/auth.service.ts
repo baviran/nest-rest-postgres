c265c142c7c7e1ff3487adb1b69660c56693b46a
aviranb <aviranb@jfrog.com>
2023-07-28 02:08:28 +0300

    Add prisma module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { Injectable, Post } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {}
 
   signIn() {}
