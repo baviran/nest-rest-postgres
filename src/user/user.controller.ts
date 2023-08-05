a72a21ac64c2b6bceacaa95b18686a1add1611ef
aviranb <aviranb@jfrog.com>
2023-08-05 19:24:51 +0300

    Protect users endpoint with jwt strategy

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guards';
+import { GetUser } from '../auth/decorator';
+import { User } from '@prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @Get()
-  getMe() {
-    return 'user info';
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
